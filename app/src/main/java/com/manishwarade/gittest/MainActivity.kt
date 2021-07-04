32d2db83589c4eb010667c2d1a92a9c70047bf4d
Manish <manish@cellica.com>
2021-07-04 12:48:05 +0530

    hello from frirnd commit

--- app/src/main/java/com/manishwarade/gittest/MainActivity.kt
+++ app/src/main/java/com/manishwarade/gittest/MainActivity.kt
@@ -2,10 +2,13 @@ package com.manishwarade.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.sql.DriverManager.println
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("Hello from your friens")
     }
 }
